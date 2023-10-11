0e72c7aec8d622cc3549a35a68bf147ac76c63c4
Anton Yurchenko <webbymasta@gmail.com>
2023-10-11 15:40:38 +0400

    update packges

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,55 @@
-const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-const { UglifyJsPlugin } = webpack.optimize;
+const path = require("path");
+const webpack = require("webpack");
+const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const env = process.env.NODE_ENV;
-const libraryName = '[name]';
+const libraryName = "[name]";
 
 const plugins = [
   new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify(env),
+    "process.env.NODE_ENV": JSON.stringify(env),
   }),
 ];
 
 let outputFile;
-if (env === 'production') {
-  plugins.push(new UglifyJsPlugin({
-    sourceMap: true,
-    compress: {
-      warnings: true,
-    },
-  }));
-  plugins.push(new webpack.LoaderOptionsPlugin({
-    minimize: true,
-  }));
+if (env === "production") {
   outputFile = `${libraryName}.min.js`;
-  plugins.push(new ExtractTextPlugin({
-    filename: 'css/styles.min.css',
-  }));
+  plugins.push(
+    new MiniCssExtractPlugin({
+      filename: "css/styles.min.css",
+    })
+  );
 } else {
   outputFile = `${libraryName}.js`;
-  plugins.push(new ExtractTextPlugin({
-    filename: 'css/styles.css',
-  }));
+  plugins.push(
+    new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+    })
+  );
 }
 
 module.exports = {
-
   entry: {
-    'react-perfect-scrollbar': [
-      path.join(__dirname, '/src/index.js'),
-      path.join(__dirname, '/src/styles.scss'),
+    "react-perfect-scrollbar": [
+      path.join(__dirname, "/src/index.js"),
+      path.join(__dirname, "/src/styles.scss"),
     ],
   },
 
-  devtool: 'source-map',
+  devtool: "source-map",
 
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, "dist"),
     filename: outputFile,
     library: libraryName,
-    libraryTarget: 'umd',
+    libraryTarget: "umd",
     umdNamedDefine: true,
   },
 
   resolve: {
-    modules: [
-      path.join(__dirname, './'),
-      'node_modules',
-    ],
-
-    extensions: ['.js', '.jsx'],
+    modules: [path.join(__dirname, "./"), "node_modules"],
+    extensions: [".js", ".jsx"],
   },
 
   module: {
@@ -69,62 +58,68 @@ module.exports = {
         test: /\.(jsx|js)$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
         },
       },
       {
         test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                sourceMap: true,
-                minimize: false,
-              },
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
             },
-            {
-              loader: 'postcss-loader',
-              options: {
-                sourceMap: true,
-              },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              sourceMap: true,
             },
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true,
-              },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
             },
-          ],
-        }),
+          },
+        ],
       },
+    ],
+  },
+
+  optimization: {
+    minimize: env === "production",
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
     ],
   },
 
   externals: [
     {
       react: {
-        root: 'React',
-        commonjs2: 'react',
-        commonjs: 'react',
-        amd: 'react',
+        root: "React",
+        commonjs2: "react",
+        commonjs: "react",
+        amd: "react",
       },
     },
     {
-      'react-dom': {
-        root: 'ReactDOM',
-        commonjs2: 'react-dom',
-        commonjs: 'react-dom',
-        amd: 'react-dom',
+      "react-dom": {
+        root: "ReactDOM",
+        commonjs2: "react-dom",
+        commonjs: "react-dom",
+        amd: "react-dom",
       },
     },
     {
-      'prop-types': {
-        root: 'PropTypes',
-        commonjs2: 'prop-types',
-        commonjs: 'prop-types',
-        amd: 'prop-types',
+      "prop-types": {
+        root: "PropTypes",
+        commonjs2: "prop-types",
+        commonjs: "prop-types",
+        amd: "prop-types",
       },
     },
   ],
